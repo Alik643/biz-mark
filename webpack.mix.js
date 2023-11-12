d66b70a6d5e8bf8245242f7acc89c6ef2d9f5b9c
Alik <alikkhachatryan@apollon.am>
2023-11-13 00:08:31 +0400

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+let mix = require('laravel-mix');
+mix.webpackConfig({
+    stats: {
+        children: true,
+    },
+});
+mix.js('resources/js/app.js', 'public/js')
+    .css('resources/css/app.css', 'public/css');
